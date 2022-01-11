e1ce5c62af4f81b106bf48205ed3702523d526b4
Megan Willis-Jackson <mwillisj@gmail.com>
2022-01-10 21:39:01 -0500

    Shiny updates

--- NOBO_shinyapp.R
+++ NOBO_shinyapp.R
@@ -1,22 +1,45 @@
 library(shiny)
 library(leaflet)
 library(leaflet.extras)
+library(qdapTools)
 
 ui <- fluidPage(
-  leafletOutput("map",
-                height = 600,
-                width = '100%'),
+  mainPanel(
+    leafletOutput("map",
+                  height = 600,
+                  width = '100%')),
   absolutePanel(
     id = "controls",
     top = 40, left = 40
   ),
-  sidebarPanel()
+  sidebarPanel(
+    selectInput("state", label = h3("Select State"), 
+                choices = GCT_and_Geographies$STATE),
+    hr(),
+    htmlOutput("goals")
+    
+  )
 )
 
+
 server <- function(input, output, session) {
-  output$map <- renderLeaflet({
+  output$goals <- renderUI({
+    x <- paste0(GCT_and_Geographies %>%
+                  as.data.frame() %>%
+                  filter(STATE == {input$state}) %>%
+                  subset(select = "label"))
+    HTML(x)
+    })
+    output$map <- renderLeaflet({
     nobo_and_gct_leaflet
   })
 }
 
 shinyApp(ui, server)
+
+
+
+
+
+
+
