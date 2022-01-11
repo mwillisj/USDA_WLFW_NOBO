87ca177c1d92c76a47e35a615ee2a8f78daa1d4e
Megan Willis-Jackson <mwillisj@gmail.com>
2022-01-11 01:08:57 -0500

    shiny updates

--- NOBO_shinyapp.R
+++ NOBO_shinyapp.R
@@ -1,25 +1,71 @@
 library(shiny)
 library(leaflet)
 library(leaflet.extras)
+library(tidyverse)
+
+bird_df <- bird_data %>%
+  as.data.frame() %>%
+  mutate(STATE = substr(bird_df$strat, 4, nchar(bird_df$strat) - 3),
+         REGION = ifelse(STATE %in% c("IL",
+                                      "IN",
+                                      "IA",
+                                      "KS",
+                                      "MN",
+                                      "MO",
+                                      "NE",
+                                      "TX"), "Central", 
+                  ifelse(STATE %in% c("DE",
+                                      "MD",
+                                      "NJ",
+                                      "OH",
+                                      "WV"), "Northeast", "Southeast")))
+
+# create a function to plot data in bottom panel of shiny app
+depth_plot <- function(var_x, var_y, var_z = "REGION") {
+  ggplot(bird_df) +
+    geom_point(aes(.data[[var_x]], .data[[var_y]],
+                   color = .data[[var_z]],
+                   fill = .data[[var_z]]),
+               alpha = 0.3, size = 3, shape = "circle") +
+    coord_flip() +
+    # labs(x = "", y = y_axis_lab, subtitle = subtitle) +
+    theme(
+      legend.position = "bottom",
+      axis.title = element_text(size = 16),
+      axis.text.x = element_text(family = "Trebuchet MS", size = 12),
+      panel.grid = element_blank())
+}
+
+# test plot
+depth_plot("STATE", "abund")
+
+
+# Shiny App ----------------------------
+## UI ---------
 
 ui <- fluidPage(
   mainPanel(
     leafletOutput("map",
                   height = 400,
-                  width = '100%')),
+                  width = '100%'),
+    selectInput("select", label = h5("Continuous Variable"),
+                choices = colnames(bird_df)),
+    plotOutput("Plot")),
   # absolutePanel(
   #   id = "controls",
-  #   top = 40, left = 40
+  #   top = 40, left = 40,
+  #   selectInput("select", label = h3("Continuous Variable"),
+  #               choices = colnames(bird_df))
   # ),
   sidebarPanel(
-    selectInput("state", label = h3("Select State"), 
+    selectInput("state", label = h5("Select State"), 
                 choices = GCT_and_Geographies$STATE),
     htmlOutput("goals")
     
   )
 )
 
-
+## Server -----------
 
 server <- function(input, output, session) {
   output$goals <- renderUI({
@@ -33,10 +79,14 @@ server <- function(input, output, session) {
                                     subset(select = "label")) - 2))
     HTML(x)
     })
+  output$Plot <- renderPlot(depth_plot("STATE", input$select))
     output$map <- renderLeaflet({
     nobo_and_gct_leaflet
   })
 }
+
+
+## Run App -----------
 
 shinyApp(ui, server)
 
